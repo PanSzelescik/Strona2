33b191531b5caf3aec07b18e0d87d8a32397ff21
PanSzelescik <panszelescik@gmail.com>
2022-01-11 13:38:00 +0100

    Dodaje do bazy

--- PHP/16/skrypt.php
+++ PHP/16/skrypt.php
@@ -6,3 +6,8 @@ $miasto = $_POST['miasto'];
 $wiek = $_POST['wiek'];
 
 echo "Imię: $imie<br>Nazwisko: $nazwisko<br>Data urodzenia: $data_urodzenia<br>Miasto: $miasto<br>Wiek: $wiek";
+
+$mysqli = mysqli_connect('localhost', 'root', '', 'auto');
+$zapytanie = "INSERT INTO xxxxx.ludzie (imie, nazwisko, data_urodzenia, miasto, wiek) VALUES (\"$imie\", \"$nazwisko\", \"$data_urodzenia\", \"$miasto\", $wiek)";
+$query = mysqli_query($mysqli, $zapytanie);
+mysqli_close($mysqli);
