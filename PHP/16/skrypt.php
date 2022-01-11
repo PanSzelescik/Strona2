6adac768076e352a9683a857b91497e789ddac2e
PanSzelescik <panszelescik@gmail.com>
2022-01-11 13:18:22 +0100

    Nie wiem

---
+++ PHP/16/skrypt.php
@@ -0,0 +1,8 @@
+<?php
+$imie = $_POST['imie'];
+$nazwisko = $_POST['nazwisko'];
+$data_urodzenia = $_POST['data_urodzenia'];
+$miasto = $_POST['miasto'];
+$wiek = $_POST['wiek'];
+
+echo "Imię: $imie<br>Nazwisko: $nazwisko<br>Data urodzenia: $data_urodzenia<br>Miasto: $miasto<br>Wiek: $wiek";
